3e374cf36e48ba2ba517b978c48ea678166c4f27
Bruno Canini <brunocanini1994@gmail.com>
2023-05-26 15:24:51 +0200

    aggiunta serie

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,16 @@ export default {
   },
   mounted() {
     console.log("store", store)
-    this.movieLoaded(this.store.urlAPI)
+    this.movieLoaded(this.store.urlAPI + "a"),
+    this.serieLoaded(this.store.urlAPISeries + "a")
   },
 
   methods: {
     ricerca(){
       let indirizzo = this.store.urlAPI + this.searchContact
-      this.movieLoaded(indirizzo)
+      let indirizzo2 = this.store.urlAPISeries + this.searchContact
+      this.movieLoaded(indirizzo),
+      this.serieLoaded(indirizzo2)
     },
 
     movieLoaded(pippo){
@@ -29,7 +32,16 @@ export default {
     }).catch(error => {
       console.error("te pareva");
     })
-    }
+    },
+
+    serieLoaded(pippo){
+      axios.get(pippo).then(risposta => {
+      console.log(risposta.data.results);
+      this.store.series = risposta.data.results
+    }).catch(error => {
+      console.error("te pareva");
+    })
+    },
 
   }
 }
@@ -39,16 +51,59 @@ export default {
 <template>
 
   <input type="search" v-model="searchContact" @keyup.enter="ricerca">
+  <div class="containerBox">
 
-  <div v-for="film in store.films">
-    <p>{{ film.title }}</p>
-    <p> {{ film.original_title }}</p>
-    <p>{{ film.vote_average }}</p>
-    <p>{{ film.original_language }}</p>
+    <div class="box" v-for="film in store.films">
+      <div class="boxImg">
+        <img class="poster" :src=" store.urlImg + film.poster_path " alt="">
+      </div>
+      <p>{{ film.title }}</p>
+      <p>{{ film.original_title }}</p>
+      <p>{{ film.vote_average }}</p>
+      <img :src="`https://flagcdn.com/16x12/${film.original_language}.png`" alt="">
+    </div>
+
   </div>
+  <div class="containerBox">
+
+  <div class="box" v-for="serie in store.series">
+    <div class="boxImg">
+      <img class="poster" :src=" store.urlImg + serie.poster_path " alt="">
+    </div>
+    <p>{{ serie.name }}</p>
+    <p>{{ serie.original_name }}</p>
+    <p>{{ serie.vote_average }}</p>
+    <img :src="`https://flagcdn.com/16x12/${serie.original_language}.png`" alt="">
+  </div>
+
+</div>
 
 </template>
 
 <style lang="scss" scoped>
+
+.box{
+  width: 170px;
+  height: 450px;
+  border: 1px solid black;
+  background-color: #cece;
+}
+
+.containerBox{
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.boxImg{
+  width: 100%;
+  height: 200px;
+}
+
+.poster{
+  width: 100%;
+  height: 100%;
+}
+
+
 
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -3,5 +3,9 @@ import { reactive } from "vue";
 export const store = reactive({
     loading: true,
     urlAPI: "https://api.themoviedb.org/3/search/movie?api_key=e49083ee231f61f6e175c870d693bd33&query=",
+    urlAPISeries:"https://api.themoviedb.org/3/search/tv?api_key=e49083ee231f61f6e175c870d693bd33&query=",
+    series: [],
     films: [],
+    urlImg: "https://image.tmdb.org/t/p/w342/",
+    urlFlag: "https://flagcdn.com/16x12/"
 })
